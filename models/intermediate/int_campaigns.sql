3b4bfdabaff6142e2960d3e6acf8000556a472f8
Kishwer Khaliq <khaliqkishwer@gmail.com>
2024-09-11 15:59:46 +0000

    ads campaign tables are added and also int_campaigns model

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,52 @@
+WITH adwords AS (
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name AS campaign_name,
+        ads_cost,
+        impression,
+        click
+    FROM {{ ref('stg_raw__adwords') }}
+),
+bing AS (
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name AS campaign_name,
+        ads_cost,
+        impression,
+        click
+    FROM {{ ref('stg_raw__bing') }}
+),
+facebook AS (
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name AS campaign_name,
+        ads_cost,
+        impression,
+        click
+    FROM {{ ref('stg_raw__facebook') }}
+),
+criteo AS (
+    SELECT
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name AS campaign_name,
+        ads_cost,
+        impression,
+        click
+    FROM {{ ref('stg_raw__criteo') }}
+)
+
+SELECT * FROM adwords
+UNION ALL
+SELECT * FROM bing
+UNION ALL
+SELECT * FROM facebook
+UNION ALL
+SELECT * FROM criteo
